722f4445c2d0cff3dea22cb1ca20b2a06798cd1d
strange_v <strange.rand@gmail.com>
2022-12-04 12:08:51 +0200

    Support different users

--- install.sh
+++ install.sh
@@ -6,21 +6,24 @@ if [ "$EUID" -ne 0 ]
 fi
 
 CURRENT_DIR=`pwd`
+USER=`id -nu 1000`
 
 cd "$(dirname "$0")"
 
 apt-get install -y supervisor
 
-mkdir /home/pi/fan
+mkdir /home/$USER/fan
 mkdir /var/log/fan
 
-cp ./fan.py /home/pi/fan/fan.py
+cp ./fan.py /home/$USER/fan/fan.py
 cp ./fan.conf /etc/supervisor/conf.d/fan.conf
 
-chown -R pi:pi /home/pi/fan
-chown -R pi:pi /var/log/fan
+sed -i "s/{USER}/$USER/" /etc/supervisor/conf.d/fan.conf
+
+chown -R $USER:$USER /home/$USER/fan
+chown -R $USER:$USER /var/log/fan
 
 supervisorctl reread
 supervisorctl update
 
-cd CURRENT_DIR
+cd $CURRENT_DIR
